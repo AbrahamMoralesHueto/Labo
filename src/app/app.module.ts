5cb2832e84923416f52e4600fbca1356457598b7
Abraham <abraham.morales.hueto@gmail.com>
2021-10-01 11:59:17 -0500

    Creacion de algunos Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { ClienteComponent } from './components/cliente/cliente.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NavComponent } from './components/nav/nav.component';
